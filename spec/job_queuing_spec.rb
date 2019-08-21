51012c63ecbb7695af0d0c8a60f2d3b827ac0ab4
Evandro <evandrohr@eopt.com.br>
2019-08-21 16:57:19 -0300

    4th and 5th requirements solved. Now the recurrent sorting by precedence algoritm is implemented.

--- spec/job_queuing_spec.rb
+++ spec/job_queuing_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe JobQueing do
   let(:jobs_empty) { JobQueing.new("") }
   let(:one_job_without_precedence) { JobQueing.new("a") }
   let(:three_jobs_without_precedence) { JobQueing.new("a,b,c") }
-
+  let(:jobs_with_precedence) { JobQueing.new("a,b=>c,c=>f,d=>a,e=>b,f") }
 
   # Receives ""
   # Returns ""
@@ -26,10 +26,10 @@ RSpec.describe JobQueing do
     expect(three_jobs_without_precedence.ordered_jobs).to_not match('[d-z]')
   end
 
-  # Receives "a,b=>c,c"
-  # Returns "a,c,b"
+  # Receives "a,b=>c,c=>f,d=>a,e=>b,f"
+  # Returns "a,f,c,b,d,e"
   it "must returns a string with jobs ordered considering precedence" do
-  
+    expect(jobs_with_precedence.ordered_jobs).to eq("a,f,c,b,d,e")
   end
 
   # Receives "a,b,c=>c"
@@ -42,6 +42,28 @@ RSpec.describe JobQueing do
   # Returns "Error: Jobs cannot have circular dependencies"
   it "returns an error when find circular dependency" do
   
+  end
+
+  it "has ability to mark a job as ordered" do
+    job = Job.new("a","b",false)
+    jobs_with_precedence.mark_as_ordered(job)
+    obj = jobs_with_precedence.jobs.select { |job_id|  job_id.id == job.id }
+    expect(obj[0].ordered).to eq(true) 
+  end
+
+  it "has ability to return one job when search for precedence but the job doesn't have one" do
+    job = Job.new("a","",false)
+    expect(one_job_without_precedence.get_precedences([job])).to eq([job])
+
+    job = Job.new("a","",true)
+    expect(one_job_without_precedence.get_precedences([job])).to be_nil
+  end
+
+  it "can return an array of precedences including the actual job" do
+    job = Job.new("b","c",false)
+    precedences = jobs_with_precedence.get_precedences([job])
+    str_precedences = precedences.map { |j| j.id}.join(',')
+    expect(str_precedences).to eq("f,c,b")
   end
 
 end
--- src/job_queuing.rb
+++ src/job_queuing.rb
@@ -1,9 +1,59 @@
+require 'byebug'
+
+Job = Struct.new(:id,:precedence,:ordered)
+
 class JobQueing
+  attr_reader :jobs
+
   def initialize(jobs)
-    @jobs = jobs
+    @jobs = []
+    unordered_jobs = jobs.split(',')
+    unordered_jobs.each do |job|
+      @jobs << Job.new(job.split("=>")[0], job.split("=>")[1].to_s, false)
+    end
   end
 
   def ordered_jobs
-    @jobs
+    ordered = []
+    @jobs.each do |job|
+      next if job.ordered == true 
+      get_precedences([job]).each do |precedence|
+        ordered << precedence.id if precedence != nil
+      end
+    end
+
+    ordered.join(',')
   end
+
+  def get_precedences(job)
+
+    # byebug
+
+    if job[0].ordered == true
+      return nil
+    end
+
+    if job[0].precedence == "" then
+      mark_as_ordered(job[0])
+      return job
+    end
+
+    precedent_job = @jobs.select { |job_id|  job_id.id == job[0].precedence }
+   
+    if precedent_job[0].ordered == false then
+      job.unshift(precedent_job[0])
+      mark_as_ordered(job[1])
+      get_precedences(job)      
+    end
+
+    mark_as_ordered(job[0])
+
+    return job
+  end
+
+  def mark_as_ordered(job)
+    obj = @jobs.select { |job_id|  job_id.id == job.id.to_s }
+    obj[0].ordered = true
+  end
+
 end
