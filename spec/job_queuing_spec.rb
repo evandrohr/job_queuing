1c921982cbada0d397b1ba1efccc632b0317d28e
Evandro <evandrohr@eopt.com.br>
2019-08-21 17:17:06 -0300

    6th requirement implemented, the class is self-references aware now. Some methods and variables refatorations too.

--- spec/job_queuing_spec.rb
+++ spec/job_queuing_spec.rb
@@ -5,37 +5,38 @@ RSpec.describe JobQueing do
   let(:one_job_without_precedence) { JobQueing.new("a") }
   let(:three_jobs_without_precedence) { JobQueing.new("a,b,c") }
   let(:jobs_with_precedence) { JobQueing.new("a,b=>c,c=>f,d=>a,e=>b,f") }
+  let(:jobs_referring_themselves) { JobQueing.new("a,b,c=>c") }
 
   # Receives ""
   # Returns ""
   it "must returns an empty string when receives an empty string" do
-    expect(jobs_empty.ordered_jobs).to eq("")
+    expect(jobs_empty.sorted_jobs).to eq("")
   end
 
   # Receives "a"
   # Returns "a"
   it "must returns a single character string when receives only one job" do
-    expect(one_job_without_precedence.ordered_jobs).to eq("a")
-    expect(one_job_without_precedence.ordered_jobs).to_not match('[b-z]')
+    expect(one_job_without_precedence.sorted_jobs).to eq("a")
+    expect(one_job_without_precedence.sorted_jobs).to_not match('[b-z]')
   end
 
   # Receives "a,b,c"
   # Returns "a,b,c"
   it "must returns a string with the jobs in no significant order when receives only jobs without precedence" do
-    expect(three_jobs_without_precedence.ordered_jobs).to include("a").and include("b").and include("c")
-    expect(three_jobs_without_precedence.ordered_jobs).to_not match('[d-z]')
+    expect(three_jobs_without_precedence.sorted_jobs).to include("a").and include("b").and include("c")
+    expect(three_jobs_without_precedence.sorted_jobs).to_not match('[d-z]')
   end
 
   # Receives "a,b=>c,c=>f,d=>a,e=>b,f"
   # Returns "a,f,c,b,d,e"
-  it "must returns a string with jobs ordered considering precedence" do
-    expect(jobs_with_precedence.ordered_jobs).to eq("a,f,c,b,d,e")
+  it "must returns a string with jobs sorted considering precedence" do
+    expect(jobs_with_precedence.sorted_jobs).to eq("a,f,c,b,d,e")
   end
 
   # Receives "a,b,c=>c"
   # Returns "Error: Jobs cannot depends on themselves"
   it "returns an error when a job depends on themself" do
-  
+    expect(jobs_referring_themselves.sorted_jobs).to eq("Error: Jobs cannot depends on themselves")
   end
 
   # Receives "a,b=>c,c=>f,d=>a,e,f=>b"
@@ -44,11 +45,11 @@ RSpec.describe JobQueing do
   
   end
 
-  it "has ability to mark a job as ordered" do
+  it "has ability to mark a job as sorted" do
     job = Job.new("a","b",false)
-    jobs_with_precedence.mark_as_ordered(job)
+    jobs_with_precedence.mark_as_sorted(job)
     obj = jobs_with_precedence.jobs.select { |job_id|  job_id.id == job.id }
-    expect(obj[0].ordered).to eq(true) 
+    expect(obj[0].sorted).to eq(true) 
   end
 
   it "has ability to return one job when search for precedence but the job doesn't have one" do
--- src/job_queuing.rb
+++ src/job_queuing.rb
@@ -1,59 +1,71 @@
 require 'byebug'
 
-Job = Struct.new(:id,:precedence,:ordered)
+Job = Struct.new(:id,:precedence,:sorted)
 
 class JobQueing
   attr_reader :jobs
 
   def initialize(jobs)
     @jobs = []
-    unordered_jobs = jobs.split(',')
-    unordered_jobs.each do |job|
+    unsorted_jobs = jobs.split(',')
+    unsorted_jobs.each do |job|
       @jobs << Job.new(job.split("=>")[0], job.split("=>")[1].to_s, false)
     end
   end
 
-  def ordered_jobs
-    ordered = []
+  def sorted_jobs
+    return "Error: Jobs cannot depends on themselves" if have_autoreference?
+    sorted = []
     @jobs.each do |job|
-      next if job.ordered == true 
+      next if job.sorted == true 
       get_precedences([job]).each do |precedence|
-        ordered << precedence.id if precedence != nil
+        sorted << precedence.id if precedence != nil
       end
     end
 
-    ordered.join(',')
+    sorted.join(',')
   end
 
   def get_precedences(job)
 
     # byebug
 
-    if job[0].ordered == true
+    if job[0].sorted == true
       return nil
     end
 
     if job[0].precedence == "" then
-      mark_as_ordered(job[0])
+      mark_as_sorted(job[0])
       return job
     end
 
     precedent_job = @jobs.select { |job_id|  job_id.id == job[0].precedence }
    
-    if precedent_job[0].ordered == false then
+    if precedent_job[0].sorted == false then
       job.unshift(precedent_job[0])
-      mark_as_ordered(job[1])
+      mark_as_sorted(job[1])
       get_precedences(job)      
     end
 
-    mark_as_ordered(job[0])
+    mark_as_sorted(job[0])
 
     return job
   end
 
-  def mark_as_ordered(job)
+  def mark_as_sorted(job)
     obj = @jobs.select { |job_id|  job_id.id == job.id.to_s }
-    obj[0].ordered = true
+    obj[0].sorted = true
+  end
+
+  def have_autoreference?
+    autoreferences = false
+    @jobs.each do |job|
+      if job.id == job.precedence.to_s then
+        autoreferences = true
+        break
+      end
+    end
+    autoreferences
   end
 
 end
