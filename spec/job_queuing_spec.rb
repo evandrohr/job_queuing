bbf95d430f5c628d2b4afd0b580b6b47cd423f05
Evandro <evandrohr@eopt.com.br>
2019-08-21 13:06:13 -0300

    Solving the 3th requirement: Given 3 jobs without precedence must return the same 3 jobs in no significant order

--- spec/job_queuing_spec.rb
+++ spec/job_queuing_spec.rb
@@ -2,7 +2,8 @@ require 'job_queuing'
 
 RSpec.describe JobQueing do
   let(:jobs_empty) { JobQueing.new("") }
-  let (:one_job_without_precedence) { JobQueing.new("a") }
+  let(:one_job_without_precedence) { JobQueing.new("a") }
+  let(:three_jobs_without_precedence) { JobQueing.new("a,b,c") }
 
 
   # Receives ""
@@ -15,12 +16,14 @@ RSpec.describe JobQueing do
   # Returns "a"
   it "must returns a single character string when receives only one job" do
     expect(one_job_without_precedence.ordered_jobs).to eq("a")
+    expect(one_job_without_precedence.ordered_jobs).to_not match('[b-z]')
   end
 
   # Receives "a,b,c"
   # Returns "a,b,c"
   it "must returns a string with the jobs in no significant order when receives only jobs without precedence" do
-
+    expect(three_jobs_without_precedence.ordered_jobs).to include("a").and include("b").and include("c")
+    expect(three_jobs_without_precedence.ordered_jobs).to_not match('[d-z]')
   end
 
   # Receives "a,b=>c,c"
